bb58e47ec52b8378f64c850154e561e9e63eafab
iamgiribabu <girianisetti06@gmail.com>
2025-09-28 19:21:31 +0530

    Add Authentication to gateway config

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"]; // IdentityServer URL
+        options.RequireHttpsMetadata = false; // For development only
+        options.TokenValidationParameters.ValidateAudience = false; // Disable audience validation
+        options.TokenValidationParameters.NameClaimType = "username"; // Set the name claim type
+    });
+
 var app = builder.Build();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 app.Run();
