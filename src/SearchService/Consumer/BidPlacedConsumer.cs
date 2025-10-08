f74895e46694ee3d98cf6416a69dee2ef7a91dd4
iamgiribabu <girianisetti06@gmail.com>
2025-10-08 23:18:09 +0530

    adding a new client to the identity service configuration

---
+++ src/SearchService/Consumer/BidPlacedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumer
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("BidPlacedConsumer received a message: " +
+                $"{context.Message.Id}, {context.Message.AuctionId}, {context.Message.Bidder}, {context.Message.Amount}, {context.Message.BidTime}, {context.Message.BidStatus}");
+        
+        
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if(context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auction.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await auction.SaveAsync();
+            }
+        }
+    }
+}
